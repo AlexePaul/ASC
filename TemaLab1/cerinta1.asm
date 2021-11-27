c304ff959bac44f40564b8a8192447e3c06f7013
AlexePaul <alexepaul2011@yahoo.com>
2021-11-27 21:20:07 +0200

    Able To Store Number In Base 10

--- TemaLab1/cerinta1.asm
+++ TemaLab1/cerinta1.asm
@@ -32,9 +32,30 @@ movb (%edi, %ecx, 1), %al
 cmp $64, %al
 jl if1
 if1r:
+cmp $64, %al
+jg if2
+if2r:
 incl %ecx
-pushl %ecx
 
+movb (%edi, %ecx, 1), %al
+cmp $64, %al
+jl if3
+if3r:
+cmp $64, %al
+jg if4
+if4r:
+incl %ecx
+
+movb (%edi, %ecx, 1), %al
+cmp $64, %al
+jl if5
+if5r:
+cmp $64, %al
+jg if6
+if6r:
+incl %ecx
+
+pushl %ecx
 pushl suma
 pushl $formatPrintf
 call printf
@@ -43,7 +64,8 @@ popl %ebx
 popl %ecx
 
 cmp %ecx, length
-jg forEt
+jle exit
+jmp forEt
 
 exit:
 movl $1, %eax
@@ -58,3 +80,54 @@ subl $48, %ebx
 mull %ebx
 movl %eax,suma
 jmp if1r
+
+if2:
+xorl %ebx,%ebx
+movl $256, %eax
+movb (%edi, %ecx, 1), %bl
+subl $55, %ebx
+mull %ebx
+movl %eax,suma
+jmp if2r
+
+if3:
+xorl %ebx,%ebx
+movl $16, %eax
+movb (%edi, %ecx, 1), %bl
+subl $48, %ebx
+mull %ebx
+addl suma, %eax
+movl %eax, suma
+jmp if3r
+
+if4:
+xorl %ebx,%ebx
+movl $16, %eax
+movb (%edi, %ecx, 1), %bl
+subl $55, %ebx
+mull %ebx
+addl suma, %eax
+movl %eax, suma
+jmp if4r
+
+if5:
+xorl %ebx,%ebx
+movl $1, %eax
+movb (%edi, %ecx, 1), %bl
+subl $48, %ebx
+mull %ebx
+addl suma, %eax
+movl %eax, suma
+xorl %eax, %eax
+jmp if5r
+
+if6:
+xorl %ebx,%ebx
+movl $1, %eax
+movb (%edi, %ecx, 1), %bl
+subl $55, %ebx
+mull %ebx
+addl suma, %eax
+movl %eax, suma
+xorl %eax, %eax
+jmp if6r
